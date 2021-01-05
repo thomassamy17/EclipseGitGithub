891ca0752d0ab2d718dc3648acf6e492d56c61e5
Thomas SAMY <tsamy@snapp.fr>
2021-01-05 14:37:22 +0100

    first commit

---
+++ src/main/java/epsi/EclispeGitGithub/App.java
@@ -0,0 +1,17 @@
+package epsi.EclispeGitGithub;
+
+public class App {
+	
+    public static void main(String[] args){
+    	new App().start();
+    }
+    
+    public App() {
+    	
+	}
+    
+    public void start() {
+
+	}
+    
+}
